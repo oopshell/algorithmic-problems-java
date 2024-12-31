2872622b09040cd04996ab4eb6536bd601f04f5d
Tiantian Li <tiantiancscs@gmail.com>
2024-12-31 19:31:12 +1100

    Update MajorityElement.java

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -19,13 +19,12 @@ public class MajorityElement {
 
     // use HashMap
     public static int majorityElement2(int[] nums) {
-        int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         for (int key : map.keySet()) {
-            if (map.get(key) > n / 2) {
+            if (map.get(key) > nums.length / 2) {
                 return key;
             }
         }
