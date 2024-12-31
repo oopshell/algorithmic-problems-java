279c77be668074daa28ecfa0bfe22f77a13dd75a
Tiantian Li <tiantiancscs@gmail.com>
2024-12-31 21:45:16 +1100

    Update RotateArray.java

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -1,7 +1,26 @@
 // 189. Rotate Array
 
+import java.util.Arrays;
+
 public class RotateArray {
+    public static void rotate1(int[] nums, int k) {
+        int n = nums.length;
+        int i = 0;
+        int start = nums[0];
+        int buffer = start;
+        while (buffer != start || i == 0) {
+            int position = (i + k) % n;
+            int tmp = nums[position];
+            nums[position] = buffer;
+            buffer = tmp;
+            i = position;
+        }
+    }
+    // Cyclic replacements, Space complexity: O(1)
+
     public static void main(String[] args) {
-        System.out.println(0);
+        int[] nums = {1,2,3,4,5,6,7};
+        rotate1(nums, 3);
+        System.out.println(Arrays.toString(nums));
     }
 }
