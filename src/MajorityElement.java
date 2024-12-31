0d5e00b099f74246e13cddf08fbdac170659e93f
Tiantian Li <tiantiancscs@gmail.com>
2024-12-31 19:15:01 +1100

    Update MajorityElement.java

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,5 +1,7 @@
 // 169. Majority Element
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MajorityElement {
     // use built-in sort
@@ -7,10 +9,28 @@ public class MajorityElement {
         Arrays.sort(nums);
         return nums[nums.length / 2];
     }
+    // Time complexity: O(nlogn)
+    // Space complexity: O(logn)
+    // java.util.Arrays.sort uses a Dual-Pivot Quicksort algorithm for primitive types (such as int, char, etc.)
+    // and a modified Timsort algorithm for objects.
 
     // use HashMap
+    public static int majorityElement2(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) > n / 2) {
+                return key;
+            }
+        }
+        return 0;  // If no majority element is found in the hash map, the code returns 0 as the default value
+    }
 
     // use Boyer-Moore Voting Algorithm
+    
 
     public static void main(String[] args) {
         int[] nums = {3, 2, 3};
