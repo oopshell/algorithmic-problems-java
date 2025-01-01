2ee5b00bb462d2d7382340504928d7846a1732fa
Tiantian Li <tiantiancscs@gmail.com>
2025-01-02 10:56:37 +1100

    Update BestTimeBuySellStock.java

--- src/BestTimeBuySellStock.java
+++ src/BestTimeBuySellStock.java
@@ -3,16 +3,21 @@
 public class BestTimeBuySellStock {
     public static int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
+        int minPriceDate = 0;
         int maxProfitValue = 0;
+        int maxProfitBuyingDate = 0;
         for (int i = 0; i < prices.length; i++) {
             int currentPrice = prices[i];
             if (currentPrice < minPrice) {
                 minPrice = prices[i];
+                minPriceDate = i + 1;
             }
             if (currentPrice - minPrice > maxProfitValue) {
                 maxProfitValue = currentPrice - minPrice;
+                maxProfitBuyingDate = minPriceDate;
             }
         }
+        System.out.println("Buying stock on Day " + maxProfitBuyingDate + " can get max profit, which is " + maxProfitValue);
         return maxProfitValue;
     }
 
