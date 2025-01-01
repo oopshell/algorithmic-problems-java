4e2096b730e2849209db920117f218fec4257987
Tiantian Li <tiantiancscs@gmail.com>
2025-01-02 10:55:20 +1100

    Update BestTimeBuySellStock.java

--- src/BestTimeBuySellStock.java
+++ src/BestTimeBuySellStock.java
@@ -4,4 +4,9 @@ public class BestTimeBuySellStock {
     public static int maxProfit(int[] prices) {
         return 0;
     }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        maxProfit(prices);
+    }
 }
