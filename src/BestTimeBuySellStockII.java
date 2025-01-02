7b6d48a9b9b2376a21c482429bced1d38a92a902
Tiantian Li <tiantiancscs@gmail.com>
2025-01-02 16:31:02 +1100

    Update BestTimeBuySellStockII.java

--- src/BestTimeBuySellStockII.java
+++ src/BestTimeBuySellStockII.java
@@ -2,20 +2,32 @@
 
 public class BestTimeBuySellStockII {
     public static int maxProfit(int[] prices) {
-        int maxProfitValue = 0;
-        int minPrice = Integer.MAX_VALUE;
-        int share = 0;
+        int totalProfit = 0;
+        int localMaxProfit = 0;
+        int localMinPrice = Integer.MAX_VALUE;
+        int localMaxPrice = Integer.MIN_VALUE;
         for (int i = 0; i < prices.length; i++) {
             int currentPrice = prices[i];
-            if (currentPrice < minPrice)
-                minPrice = currentPrice;
-            else
-                share = 1;
-            if (currentPrice - minPrice > maxProfitValue) {
-                maxProfitValue = currentPrice - minPrice;
+            if (currentPrice < localMinPrice) {
+                localMinPrice = currentPrice;
+            }
+            else {
+                if (currentPrice >= localMaxPrice) {
+                    int profit = currentPrice - localMinPrice;
+                    if (profit > localMaxProfit) {
+                        localMaxProfit = profit;
+                        localMaxPrice = currentPrice;
+                    }
+                }
+                else {
+                    totalProfit += localMaxProfit;
+                    localMaxProfit = 0;
+                    localMinPrice = currentPrice;
+                    localMaxPrice = Integer.MIN_VALUE;
+                }
             }
         }
-        return maxProfitValue;
+        return totalProfit;
     }
 
     public static void main(String[] args) {
