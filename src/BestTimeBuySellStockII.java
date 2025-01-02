f04e4ebf86e88642f8e49b6618f667591dbc2a2f
Tiantian Li <tiantiancscs@gmail.com>
2025-01-02 16:52:40 +1100

    Update BestTimeBuySellStockII.java

--- src/BestTimeBuySellStockII.java
+++ src/BestTimeBuySellStockII.java
@@ -6,10 +6,12 @@ public class BestTimeBuySellStockII {
         int localMaxProfit = 0;
         int localMinPrice = Integer.MAX_VALUE;
         int localMaxPrice = Integer.MIN_VALUE;
+        int share = 0;
         for (int currentPrice : prices) {
-            if (currentPrice < localMinPrice) {
+            if (currentPrice <= localMinPrice && share == 0) {
                 localMinPrice = currentPrice;
             } else {
+                share = 1;
                 if (currentPrice >= localMaxPrice) {
                     int profit = currentPrice - localMinPrice;
                     if (profit > localMaxProfit) {
@@ -17,6 +19,7 @@ public class BestTimeBuySellStockII {
                         localMaxPrice = currentPrice;
                     }
                 } else {
+                    share = 0;
                     totalProfit += localMaxProfit;
                     localMaxProfit = 0;
                     localMinPrice = currentPrice;
@@ -24,11 +27,17 @@ public class BestTimeBuySellStockII {
                 }
             }
         }
+        if (share == 1)
+            totalProfit += localMaxProfit;
         return totalProfit;
     }
 
     public static void main(String[] args) {
-        int[] prices = {7,1,5,3,6,4};
-        System.out.println(maxProfit(prices));
+//        int[] prices = {7,1,5,3,6,4};
+//        int[] prices = {1,2,3,4,5};
+//        int[] prices = {6,1,3,2,4,7};
+//        int[] prices = {2,1,2,0,1};
+        int[] prices = {8,6,4,3,3,2,3,5,8,3,8,2,6};
+                System.out.println(maxProfit(prices));
     }
 }
