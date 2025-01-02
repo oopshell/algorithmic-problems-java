02279bad8c03d4f52639cbd23700beb338698918
Tiantian Li <tiantiancscs@gmail.com>
2025-01-02 11:05:06 +1100

    Update BestTimeBuySellStockII.java

--- src/BestTimeBuySellStockII.java
+++ src/BestTimeBuySellStockII.java
@@ -1,4 +1,13 @@
 // 122. Best Time to Buy and Sell Stock II
 
 public class BestTimeBuySellStockII {
+    public static int maxProfit(int[] prices) {
+        int maxProfitValue = 0;
+        return maxProfitValue;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
 }
