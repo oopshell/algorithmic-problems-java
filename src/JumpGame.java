d2778b9cda532c259137a50fc783e67bbb690c7d
Tiantian Li <tiantiancscs@gmail.com>
2025-01-07 23:11:12 +1100

    Update JumpGame.java

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -2,5 +2,8 @@
 
 public class JumpGame {
   public static void main(String[] args) {
+  	// Create a HashMap object called capitalCities
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
   }
 }
